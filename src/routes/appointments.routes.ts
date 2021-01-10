d1474e84ba817472eebd7c82b61c47d12887507c
VarnerCardoso <varner.silveira@gmail.com>
2021-01-10 18:41:33 -0300

    first commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+
+// Rota GET
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
+
+  return response.json(appointments);
+});
+
+// Rota POST
+appointmentsRouter.post('/', async (request, response) => {
+  try {
+    const { provider_id, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider_id,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ Error: err.message });
+  }
+});
+
+export default appointmentsRouter;
